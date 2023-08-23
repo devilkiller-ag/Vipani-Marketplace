ad9b27ec76ba815b0f8876aeb502465efbffa282
devilkiller-ag <ashmitgupta.official@gmail.com>
2023-08-24 02:18:09 +0530

    created navbar

---
+++ components/navbar/navbar.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+
+import Container from "@/components/ui/container";
+import MainNav from "@/components/navbar/main-nav";
+import getCategories from "@/actions/get-categories";
+import NavbarActions from "./navbar-actions";
+
+export const revalidate = 0; // So that navbar is never cached
+
+const Navbar = async () => {
+
+  const categories = await getCategories();
+
+  return (
+    <div className='border-b'>
+      <Container>
+        <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
+          <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
+            <p className="font-bold text-xl">vipaṇi</p>
+          </Link>
+          <MainNav data={categories} />
+          <NavbarActions />
+        </div>
+      </Container>
+    </div>
+  )
+}
+
+export default Navbar
